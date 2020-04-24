b4759683d92ca59ed0e83e9173895316b440b249
Henrik Bengtsson <hb@aroma-project.org>
2020-04-24 13:26:48 -0700

    Add --as=cran::ci [#3]

--- R/cran.R
+++ R/cran.R
@@ -1,6 +1,15 @@
 check_cran <- function(args, stdin) {
+  if (isTRUE(args$ci)) {
+    Sys.setenv("_R_CHECK_CRAN_INCOMING_REMOTE_" = "false")
+    args$ci <- NULL
+  }
   args <- attr(args, "command_line_arguments")
   args <- c("--as-cran", args)
   list(args = args, stdin = stdin)
+}
+
+check_cran_ci <- function(args, stdin) {
+  args$ci <- TRUE
+  check_cran(args, stdin)
 }
 
--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,5 @@
   ## Register 'R CMD check' flavors
   check_as_functions("rcli-test" = check_rcli_test)
   check_as_functions("cran" = check_cran)
+  check_as_functions("cran::ci" = check_cran_ci)
 }
